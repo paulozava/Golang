9d8c6ce049b6c9a91cedaf0362e4e92a2ffe5e7c
Paulo Campos Zava <camposzava@gmail.com>
2019-05-10 22:38:39 +0200

    if else

--- hello-word.go
+++ hello-word.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
-// var message = "oi"
-// var newMessage = "1"
-// var num1, num2 = 1, 2
 var numbers = []int{1, 2, 3, 4, 5}
 
 func main() {
-	fmt.Println(numbers)
+	for index := 0; index < len(numbers); index++ {
+		number := numbers[index]
+		if number < 2 {
+			fmt.Println("2 minor")
+		} else if number == 2 {
+			fmt.Println("2 inside")
+		} else {
+			fmt.Println("Outer")
+		}
+	}
 }
