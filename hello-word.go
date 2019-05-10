dec951dffdf9b1bc71fd79883247077d7c1a2eb7
Paulo Campos Zava <camposzava@gmail.com>
2019-05-10 22:22:00 +0200

    numbers

--- hello-word.go
+++ hello-word.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
-var message = "oi"
-var newMessage = "1"
+// var message = "oi"
+// var newMessage = "1"
+// var num1, num2 = 1, 2
+var numbers = []int{1, 2, 3, 4, 5}
 
 func main() {
-	fmt.Println(newMessage)
+	fmt.Println(numbers)
 }
