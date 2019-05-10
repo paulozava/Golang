d1915e84e8927fb9a1391e75471b9aee25beca77
Paulo Campos Zava <camposzava@gmail.com>
2019-05-10 22:50:29 +0200

    functions

--- hello-word.go
+++ hello-word.go
@@ -4,10 +4,16 @@ import "fmt"
 
 var numbers = []int{1, 2, 3, 4, 5}
 
+func add(a, b int) int {
+	c := a + b
+	return c
+}
+
 func main() {
 	for index := 0; index < len(numbers); index++ {
 		number := numbers[index]
 		if number < 2 {
+
 			fmt.Println("2 minor")
 		} else if number == 2 {
 			fmt.Println("2 inside")
